783210a263b9080ee8414374e871f70bb0675742
Kristian Hannula <kristian.hannula@proton.me>
2023-09-14 15:08:02 +0300

    Task 12.9 seems to be done

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-const configs = require('../util/config')
+const redis = require('../redis');
+const configs = require('../util/config');
 
 let visits = 0
 
