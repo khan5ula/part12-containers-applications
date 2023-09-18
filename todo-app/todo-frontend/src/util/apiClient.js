f65c031d53b44ac159f979b7cf706519822fa454
Kristian Hannula <kristian.hannula@proton.me>
2023-09-18 14:10:37 +0300

    Task 12.14 done, added a test to Dockerfile build process

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL, // "http://localhost:3000"
 });
 
 export default apiClient;
